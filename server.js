fd3afbe1499ebd2b06ca7813a5a3698abcaff744
vipinsanthosh <58885397+vipinsanthosh@users.noreply.github.com>
2020-02-27 00:34:23 +0300

    fist commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+// environment variable for machine
+const port = process.env.PORT || 3000;
+// setup of view engine using express handlebars
+app.engine('handlebars',  exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/',(req,res) =>{
+    res.render('home', 
+        {title : 'Home'}
+    );
+});
+
+app.get('/about',(req,res) =>{
+    res.render('about', {title : 'About'});
+});
+
+app.get('/contact',(req,res) =>{
+    res.render('contact' , {title : 'Contact Us'});
+});
+
+app.listen(port,() =>{
+    console.log(`Server is running on port ${port}`);
+});
